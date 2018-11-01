ca69af8159871d547ea6679f15e90b6c31ce7532
chriscarr365 <cycarr@dundee.ac.uk>
2018-11-01 21:01:20 +0000

    Create Menu.cpp

---
+++ Menu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input;
+	cout << "**** MAIN MENU ****";
+	cout << "1. Run Cellular Automaton without saving to file";
+	cout << "2. Run Cellular Automaton and save to file";
+	cin >> 
+}
