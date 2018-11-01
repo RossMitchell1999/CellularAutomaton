02de9f8477bc59aa98c5faf96148ac964a600fe6
RossMitchell1999 <44398738+RossMitchell1999@users.noreply.github.com>
2018-11-01 16:51:19 +0000

    Update cellular_rules.cpp

--- cellular_rules.cpp
+++ cellular_rules.cpp
@@ -2,20 +2,28 @@
 //Cellular Automiton
 #include <iostream>
 
-int array[10]= {0,1,0,0,0,1,1,1,0,1};
-int newLine[10] = {};
+int array[31]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int newLine[31] = {};
+int rules[8] = {0,0,0,1,1,0,1,0};
 
 void getLine();
 int firstItem(int i);
 int lastItem(int i);
-int rules(int i);
+int rule(int i);
 
 int main(int argc, char const *argv[])
 {
-	getLine();
-	for ( int i = 0; i < sizeof(array)/sizeof(array[0]); ++i)
-	{
-		 std::cout<<newLine[i];
+	for (int i = 0; i < 15; ++i)
+	{	
+
+		getLine();
+		for ( int i = 0; i < sizeof(array)/sizeof(array[0]); ++i)
+		{
+			 std::cout<<array[i];
+			 array[i]=newLine[i];
+		}
+		std::cout<<"\n";
+
 	}
 	return 0;
 }
@@ -26,54 +34,131 @@ void getLine()
 	for (int i = 0; i < sizeof(array)/sizeof(array[0]); ++i){
 		if (i == 0)
 		{
-			value = firstItem (i);
+			value = firstItem(i);
 		}
 		else if (i == sizeof(array))
 		{
-		value = lastItem (i);
+		value = lastItem(i);
 		}else{
-			value = rules (i);
+			value = rule(i);
 		}
 		newLine[i] = value;
 	}
 }
 
 int firstItem(int i){
-	if (array[i] == 0)
+	if (array[(sizeof(array)/sizeof(array[0]))-1] == 0 && array[0] == 0 && array[i+1] == 0)
 	{
-		if (((array[(sizeof(array)/sizeof(array[0]))-1] == 0) && (array[i+1] == 0)) || ((array[(sizeof(array)/sizeof(array[0]))-1] == 1) && (array[i+1] == 1)))
-		{
-			std::cout<<"correct";
+		if(rules[7]==0){
 			return 0;
 		}else{
-			std::cout<<sizeof(array)-1;
-			std::cout<<array[(sizeof(array)/sizeof(array[0]))-1];
-			std::cout<<array[i+1];
-			std::cout<<"wrong";
+			return 1;
+		}
+
+	}else if (array[(sizeof(array)/sizeof(array[0]))-1] == 0 && array[0] == 0 && array[i+1] == 1)
+	{
+		if(rules[6]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[(sizeof(array)/sizeof(array[0]))-1] == 0 && array[0] == 1 && array[i+1] == 0)
+	{
+		if(rules[5]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[(sizeof(array)/sizeof(array[0]))-1] == 0 && array[0] == 1 && array[i+1] == 1)
+	{
+		if(rules[4]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[(sizeof(array)/sizeof(array[0]))-1] == 1 && array[0] == 0 && array[i+1] == 0)
+	{
+		if(rules[3]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[(sizeof(array)/sizeof(array[0]))-1] == 1 && array[0] == 0 && array[i+1] == 1)
+	{
+		if(rules[2]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[(sizeof(array)/sizeof(array[0]))-1] == 1 && array[0] == 1 && array[i+1] == 0)
+	{
+		if(rules[1]==0){
+			return 0;
+		}else{
 			return 1;
 		}
 	}else{
-		if ((array[(sizeof(array)/sizeof(array[0]))-1] == 1 && array[i+1] == 1) || (array[(sizeof(array)/sizeof(array[0]))-1] == 1 && array[i+1] == 0))
-		{
+		if(rules[0]==0){
 			return 0;
 		}else{
 			return 1;
 		}
-	}	
+	}		
 }
 
 int lastItem(int i){
-	if (array[i] == 0)
+	if (array[i-1] == 0 && array[i] == 0 && array[0] == 0)
 	{
-		if ((array[i-1] == 0 && array[0] == 0) || (array[i-1] == 1 && array[0] == 1))
-		{
+		if(rules[7]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+
+	}else if (array[i-1] == 0 && array[i] == 0 && array[0] == 1)
+	{
+		if(rules[6]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 0 && array[i] == 1 && array[0] == 0)
+	{
+		if(rules[5]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 0 && array[i] == 1 && array[0] == 1)
+	{
+		if(rules[4]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 1 && array[i] == 0 && array[0] == 0)
+	{
+		if(rules[3]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 1 && array[i] == 0 && array[0] == 1)
+	{
+		if(rules[2]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 1 && array[i] == 1 && array[0] == 0)
+	{
+		if(rules[1]==0){
 			return 0;
 		}else{
 			return 1;
 		}
 	}else{
-		if ((array[i-1] == 1 && array[0] == 1) || (array[i-1] == 1 && array[0] == 0))
-		{
+		if(rules[0]==0){
 			return 0;
 		}else{
 			return 1;
@@ -81,21 +166,63 @@ int lastItem(int i){
 	}	
 }
 
-int rules(int i){
-	if (array[i] == 0)
+int rule(int i){
+	if (array[i-1] == 0 && array[i] == 0 && array[i+1] == 0)
 	{
-		if ((array[i-1] == 0 && array[i+1] == 0) || (array[i-1] == 1 && array[i+1] == 1))
-		{
+		if(rules[7]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+
+	}else if (array[i-1] == 0 && array[i] == 0 && array[i+1] == 1)
+	{
+		if(rules[6]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 0 && array[i] == 1 && array[i+1] == 0)
+	{
+		if(rules[5]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 0 && array[i] == 1 && array[i+1] == 1)
+	{
+		if(rules[4]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 1 && array[i] == 0 && array[i+1] == 0)
+	{
+		if(rules[3]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 1 && array[i] == 0 && array[i+1] == 1)
+	{
+		if(rules[2]==0){
+			return 0;
+		}else{
+			return 1;
+		}
+	}else if (array[i-1] == 1 && array[i] == 1 && array[i+1] == 0)
+	{
+		if(rules[1]==0){
 			return 0;
 		}else{
 			return 1;
 		}
 	}else{
-		if ((array[i-1] == 1 && array[i+1] == 1) || (array[i-1] == 1 && array[i+1] == 0))
-		{
+		if(rules[0]==0){
 			return 0;
 		}else{
 			return 1;
 		}
 	}
+	
 }
