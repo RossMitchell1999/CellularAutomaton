69909c03d5e9ea685a71cd1a676aa8d3c96f694c
chriscarr365 <cycarr@dundee.ac.uk>
2018-11-01 20:44:12 +0000

    Create FileWrite.cpp

---
+++ FileWrite.cpp
@@ -0,0 +1,22 @@
+//for a file write, I'll build an option in the menu to write to file.
+//this will set a fileWrite bool to true and then run the program normally
+//but will require an additional if statement in the getLine() method that will save to file.
+//I've written a test method below and can implement this into the method 
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+void fileWrite();
+
+int main() {
+	fileWrite();
+}
+
+void fileWrite() {
+	string input;
+	cout << "enter line to be written to file";
+	cin >> input;
+	ofstream myfile;
+	myfile.open("CellularAutomatonOutput.txt");
+	myfile << input;
+}
